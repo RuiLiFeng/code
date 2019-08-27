024657605cc0c3062a9c74a7c78d0b8b80b91260
frl <frl1996@mail.ustc.edu.cn>
2019-08-27 16:33:00 +0800

    's'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from training_loop.biggandeep import training_loop
 from training_loop.training_utils import *
 import gin
+import gin.tf.external_configurables
 
 
 gin.parse_config_file('/ghome/fengrl/home/code/code/config/biggan_imagenet128.gin')
